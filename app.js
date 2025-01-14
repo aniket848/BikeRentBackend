c7f98f4700c2822ced675907d9a71b84be05cc76
aniket848 <62254194+aniket848@users.noreply.github.com>
2025-01-14 10:08:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -48,13 +48,13 @@ socketIO.on('connection',(socket)=>{
 // -------------------- DEPLOYMENT ------------------------//
 
 
-if(process.env.NODE_ENV==='production'){
+// if(process.env.NODE_ENV==='production'){
       
-    app.use(express.static(path.join(__dirname,"/client/build")));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname1,"client","build","index.html"));
-    });
-}
+//     app.use(express.static(path.join(__dirname,"/client/build")));
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname1,"client","build","index.html"));
+//     });
+// }
 
 // -------------------- DEPLOYMENT ------------------------//
 
